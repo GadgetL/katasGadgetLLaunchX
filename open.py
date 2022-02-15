e317c7dcbe758d5357ed54613dfa64e014838cfc
GadgetL <laura.ramirezcosmes@gmail.com>
2022-02-15 14:17:01 -0600

    kata 10

--- open.py
+++ open.py
@@ -1,5 +1,11 @@
 def main():
-    open("path/to/mars.jpg")
+    try:
+     open("path/to/mars.jpg")
+    except OSError as err:
+        if err.errno == 2:
+            print("Couldn't find the config.txt file!")
+        elif err.errno == 13:
+            print("Found config.txt but couldn't read it")
 
 if __name__ == '__main__':
     main()
